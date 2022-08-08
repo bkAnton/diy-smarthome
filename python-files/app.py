29353c6a35929c74510aaecfac99df3cec8418ac
bkAnton <anton.beck.erzhausen@t-online.de>
2022-08-08 20:38:24 +0200

    create new file: app.py

---
+++ python-files/app.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("500x300")
+
+def sync():
+    pass
+
+def save_settings():
+    pass
+
+def add_device():
+    pass
+
+def rename_device():
+    pass
+
+def delete_device():
+    pass
+
+def restart_interface_program():
+    pass
+root.mainloop()
